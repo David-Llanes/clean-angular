8a196428a5e4933c455051b45f9ee85954c89b8c
David Alejandro Llanes Bojorquez <david.llanes1019@gmail.com>
2025-06-01 15:08:33 -0700

    add tailwind v4

--- src/app/features/dashboard/presentation/dashboard-page.ts
+++ src/app/features/dashboard/presentation/dashboard-page.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard-page',
   imports: [],
-  template: ` <h1>Dashboard page</h1>`,
+  template: ` <h1 class="text-3xl font-bold underline">Hello world!</h1>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
